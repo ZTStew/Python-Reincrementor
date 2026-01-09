e33db0692cd6f0eaa872075d798f0212a6e7711a
ZTStew <zt.stewart.5@gmail.com>
2026-01-08 19:28:19 -0500

    Added new arguments to program

--- reincrementor.py
+++ reincrementor.py
@@ -21,6 +21,8 @@ log.basicConfig(
 log.critical("### ### ### V Program Starts V ### ### ###")
 
 start_point = 1
+file_type = "*"
+digits = 1
 
 args = argparse.ArgumentParser()
 args.add_argument(
@@ -35,6 +37,44 @@ args.add_argument(
   type=int,
   help="Declair start value for reincrementing."
 )
+args.add_argument(
+  "-f",
+  "--file",
+  type=str,
+  help="Declair a specific file type to be reincremented. Default all files."
+)
+args.add_argument(
+  "-f",
+  "--file",
+  type=str,
+  help="Declair a specific file type to be reincremented. Default all files."
+)
+args.add_argument(
+  "-d",
+  "--digits",
+  type=int,
+  help="Declair how many digits the file name should have. Default 1. Example: 3 -> `008.file`"
+)
+args.add_argument(
+  "-p",
+  "--prefix",
+  type=str,
+  help="Declair if the reincremented files should have a prefix in front of the number ([space] automatically added after). Example: pre -> `pre 008.file`"
+)
+args.add_argument(
+  "-s",
+  "--suffix",
+  type=str,
+  help="Declair if the reincremented files should have a suffix after the number ([space] automatically added before). Example: suf -> `008 suf.file`"
+)
+args.add_argument(
+  "-sp",
+  "--space",
+  type=str,
+  help="Defines what the `space` symbol should be in reincremented file name. Default ` `. Works with --prefix and/or --suffix. Example: `_` -> `008_suf.file`"
+)
+
+
 
 
 args = args.parse_args()
@@ -46,8 +86,12 @@ if args.test:
 if args.start:
   start_point = int(args.start)
 
+# NOTE need to format around `.`
+if args.file:
+  file_type = args.file
 
-
+if args.digits:
+  digits = args.digits
 
 
 
